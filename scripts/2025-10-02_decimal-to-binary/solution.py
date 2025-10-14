61c5bf063ba7766bf30b8bdb0ffc2b84d0eece9a
wqkun <phommachanhmatthieu@gmail.com>
2025-10-14 21:56:39 +0200

    test(decimal-to-binary): add example test cases for validation

--- scripts/2025-10-02_decimal-to-binary/solution.py
+++ scripts/2025-10-02_decimal-to-binary/solution.py
@@ -36,3 +36,9 @@ def to_binary(decimal):
     binary = "".join([str(num) for num in rests])[::-1]
 
     return binary
+
+# Tests
+print(to_binary(5))  # should return "101"
+print(to_binary(12)) # should return "1100"
+print(to_binary(50)) # should return "110010"
+print(to_binary(99)) # should return "1100011"
