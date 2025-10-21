1e92d90e6d73882b194dd47d2bdc2218cc1c934e
wqkun <phommachanhmatthieu@gmail.com>
2025-10-22 00:11:41 +0200

    chore(array_diff): remove test cases comments

--- scripts/2025-09-10_array_diff/solution.py
+++ scripts/2025-09-10_array_diff/solution.py
@@ -29,8 +29,8 @@ def array_diff(arr1: List[str], arr2: List[str]) -> List[str]:
     return diff
 
 # Tests
-print(array_diff(["apple", "banana"], ["apple", "banana", "cherry"]))                                  # should return ["cherry"]
-print(array_diff(["apple", "banana", "cherry"], ["apple", "banana"]))                                  # should return ["cherry"]
-print(array_diff(["one", "two", "three", "four", "six"], ["one", "three", "eight"]))                   # should return ["eight", "four", "six", "two"]
-print(array_diff(["two", "four", "five", "eight"], ["one", "two", "three", "four", "seven", "eight"])) # should return ["five", "one", "seven", "three"]
-print(array_diff(["I", "like", "freeCodeCamp"], ["I", "like", "rocks"]))                               # should return ["freeCodeCamp", "rocks"]
+print(array_diff(["apple", "banana"], ["apple", "banana", "cherry"]))
+print(array_diff(["apple", "banana", "cherry"], ["apple", "banana"]))
+print(array_diff(["one", "two", "three", "four", "six"], ["one", "three", "eight"]))
+print(array_diff(["two", "four", "five", "eight"], ["one", "two", "three", "four", "seven", "eight"]))
+print(array_diff(["I", "like", "freeCodeCamp"], ["I", "like", "rocks"]))
