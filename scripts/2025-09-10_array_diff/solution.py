4311ead6a4b13c8f169829f83a9eaa09d1f58c1c
wqkun <phommachanhmatthieu@gmail.com>
2025-10-28 00:44:30 +0100

    test(array_diff): switch to assert-based test cases for validation

--- scripts/2025-09-10_array_diff/solution.py
+++ scripts/2025-09-10_array_diff/solution.py
@@ -28,10 +28,13 @@ def array_diff(arr1: List[str], arr2: List[str]) -> List[str]:
 
     return diff
 
-# Tests
-print(array_diff(["apple", "banana"], ["apple", "banana", "cherry"]))
-print(array_diff(["apple", "banana", "cherry"], ["apple", "banana"]))
-print(array_diff(["one", "two", "three", "four", "six"], ["one", "three", "eight"]))
-print(array_diff(["two", "four", "five", "eight"],
-                 ["one", "two", "three", "four", "seven", "eight"]))
-print(array_diff(["I", "like", "freeCodeCamp"], ["I", "like", "rocks"]))
+if __name__ == "__main__":
+    assert array_diff(["apple", "banana"], ["apple", "banana", "cherry"]) == ["cherry"]
+    assert array_diff(["apple", "banana", "cherry"], ["apple", "banana"]) == ["cherry"]
+    assert array_diff(["one", "two", "three", "four", "six"],
+                      ["one", "three", "eight"]) == ["eight", "four", "six", "two"]
+    assert array_diff(["two", "four", "five", "eight"],
+                 ["one", "two", "three", "four", "seven", "eight"]) == ["five", "one", "seven", "three"]
+    assert array_diff(["I", "like", "freeCodeCamp"],
+                      ["I", "like", "rocks"]) == ["freeCodeCamp", "rocks"]
+    print("All tests passed!")
